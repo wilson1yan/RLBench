361f6507ee7f0b8372a00b668354f7c2acee6010
Wilson Yan <wilson1.yan@berkeley.edu>
2021-11-08 16:02:24 -0800

    lamp

--- rlbench/backend/const.py
+++ rlbench/backend/const.py
@@ -22,7 +22,7 @@ VARIATIONS_FOLDER = 'variation%d'
 LOW_DIM_PICKLE = 'low_dim_obs.pkl'
 VARIATION_DESCRIPTIONS = 'variation_descriptions.pkl'
 
-TTT_FILE = 'task_design.ttt'
+TTT_FILE = 'task_design_lamp.ttt'
 
 DEPTH_SCALE = 2**24 - 1
 
--- rlbench/tasks/lamp_on.py
+++ rlbench/tasks/lamp_on.py
@@ -14,7 +14,7 @@ class LampOn(Task):
         self.bulb_glass_visual.set_color([0, 0, 0])
         self.joint = Joint('target_button_joint')
         self.condition = JointCondition(self.joint, 0.003)
-        # self.register_waypoint_ability_start(1, self._add_waypoint_noise)
+        self.register_waypoint_ability_start(1, self._add_waypoint_noise)
 
     def init_episode(self, index: int) -> List[str]:
         self.bulb_glass_visual.set_color([0, 0, 0])
@@ -30,17 +30,17 @@ class LampOn(Task):
         return 1
 
     def step(self) -> None:
-        if self.condition.condition_met() == (True, True):
+        if self.condition.condition_met()[0]:
             self.bulb_glass_visual.set_color([1, 1, 1])
         
-    # def base_rotation_bounds(self):
-    #     return (0.0, 0.0, -np.pi), (0.0, 0.0, 0.0)
+    def base_rotation_bounds(self):
+        return (0.0, 0.0, -np.pi / 4), (0.0, 0.0, np.pi / 4)
 
-    # def _add_waypoint_noise(self, waypoint):
-    #     waypoint = waypoint._waypoint
-    #     print(waypoint.get_position())
-    #     if np.random.rand() < 0.25:
-    #         eps = np.zeros(3)
-    #     else:
-    #         eps = np.random.randn(3) * 0.025
-    #     waypoint.set_position(waypoint.get_position() + eps)
+    def _add_waypoint_noise(self, waypoint):
+        waypoint = waypoint._waypoint
+        if np.random.rand() < 0.25:
+            eps = np.zeros(3)
+        else:
+            eps = np.random.randn(3) * 0.01
+            eps = np.clip(eps, -0.01, 0.01)
+        waypoint.set_position(waypoint.get_position() + eps)
